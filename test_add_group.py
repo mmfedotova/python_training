584a5628a9baf246b18e42778608b4bb206bc8c2
mmfedotova <mmfedotova@phoenixit.ru>
2023-06-08 21:04:15 +0300

    add comments

--- test_add_group.py
+++ test_add_group.py
@@ -1,7 +1,8 @@
+# -*- coding: utf-8 -*-
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest
 from webdriver_manager.firefox import GeckoDriverManager
 
 
@@ -12,7 +13,9 @@ class TestAddGroup(unittest.TestCase):
 
     def test_add_group(self):
         wd = self.wd
+        # open home page
         wd.get("http://localhost/addressbook/")
+        # login
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
@@ -20,8 +23,11 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_xpath("//input[@value='Login']").click()
+        # open groups page
         wd.find_element_by_link_text("groups").click()
+        # init group creation
         wd.find_element_by_name("new").click()
+        # fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys("test")
@@ -31,9 +37,12 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys("test")
+        # submit group creation
         wd.find_element_by_name("submit").click()
+        # return to group page
         wd.find_element_by_id("header").click()
         wd.find_element_by_link_text("groups").click()
+        # logaut
         wd.find_element_by_link_text("Logout").click()
 
     def is_element_present(self, how, what):
