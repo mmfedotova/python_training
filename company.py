7db9ef4093eec82a21e390e13463f67fcb0ccfdf
mmfedotova <mmfedotova@phoenixit.ru>
2023-06-15 00:05:33 +0300

    delete classes Emails and Name and add class Contact

--- company.py
+++
@@ -1,6 +0,0 @@
-class Company:
-
-    def __init__(self, title, company, address):
-        self.title = title
-        self.company = company
-        self.address = address
---
+++ contact.py
@@ -0,0 +1,14 @@
+class Contact:
+
+    def __init__(self, firstname, middlename, lastname, nickname, email, email2, email3, homepage, title, company, address):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.email = email
+        self.email2 = email2
+        self.email3 = email3
+        self.homepage = homepage
+        self.title = title
+        self.company = company
+        self.address = address
