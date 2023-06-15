a513678c19c7811cbda0be7c258429bb618082cd
mmfedotova <mmfedotova@phoenixit.ru>
2023-06-15 23:36:19 +0300

    add classes for fixture

--- test_all/test_add_contact.py
+++ test_all/test_add_contact.py
@@ -4,7 +4,7 @@ from model.contact import Contact
 
 def test_add_contact(app):
     app.session.login()
-    app.create_contact(
+    app.contact.create(
         Contact(firstname="Sergei", middlename="Ivanovich", lastname="Smirmov", nickname="test", title="title",
                 company="shop", address="Moscow", email="test@mail.com", email2="test2@mail.com",
                 email3="test3@mail.com", homepage="test.com"))
--- test_all/test_add_group.py
+++ test_all/test_add_group.py
@@ -4,5 +4,5 @@ from model.group import Group
 
 def test_add_group(app):
     app.session.login()
-    app.create_group(Group(name="testName", header="test", footer="test"))
+    app.group.create(Group(name="testName", header="test", footer="test"))
     app.session.logout()
