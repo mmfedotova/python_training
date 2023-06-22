c69e5c426eb0062aa3e1c691b11853e5fee559b8
mmfedotova <mmfedotova@phoenixit.ru>
2023-06-23 01:18:38 +0300

    add fixture and session validation

--- test_all/test_group.py
+++ test_all/test_group.py
@@ -5,22 +5,16 @@ import time
 
 def test_add_group(app):
     time.sleep(1)
-    app.session.login()
     app.group.create_group(Group(name="testName", header="test", footer="test"))
-    app.session.logout()
 
 
 def test_edit_first_group(app):
     time.sleep(1)
-    app.session.login()
     app.group.open_groups_page()
     app.group.edit_first_group(Group(name="testName1", header="test1", footer="test1"))
-    app.session.logout()
 
 
 def test_delete_first_group(app):
     time.sleep(1)
-    app.session.login()
     app.group.open_groups_page()
     app.group.delete_first_group()
-    app.session.logout()
