281ac5182b626109fabdb5397b02ce083320e8f5
mmfedotova <mmfedotova@phoenixit.ru>
2023-07-11 15:23:28 +0300

    phones third commit

--- fixture/contact_helper.py
+++ fixture/contact_helper.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.support.ui import Select
 from model.contact import Contact
+import re
 
 
 class ContactHelper:
@@ -41,6 +42,7 @@ class ContactHelper:
 
     def open_contact_view_by_index(self, index):
         wd = self.wd
+        self.app.open_home_page()
         wd.find_elements_by_css_selector("img[title = 'Details']")[index].click()
 
     def edit_contact_by_index(self, contact, index):
@@ -165,3 +167,14 @@ class ContactHelper:
         secondaryphone = wd.find_element_by_name("phone2").get_attribute("value")
         return Contact(firstname=firstname, lastname=lastname, homephone=homephone, mobilephone=mobilephone,
                        workphone=workphone, secondaryphone=secondaryphone, id=id)
+
+    def get_contact_from_view_page(self, index):
+        wd = self.app.wd
+        self.open_contact_view_by_index(index)
+        text = wd.find_element_by_id("content").text
+        homephone = re.search("H: (.*)", text).group(1)
+        mobilephone = re.search("M: (.*)", text).group(1)
+        workphone = re.search("W: (.*)", text).group(1)
+        secondaryphone = re.search("P: (.*)", text).group(1)
+        return Contact(homephone=homephone, mobilephone=mobilephone,
+                       workphone=workphone, secondaryphone=secondaryphone)
